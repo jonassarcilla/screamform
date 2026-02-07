ee5f5fbe80e83bc6752afc79ab0aed6de9bf62de
DESKTOP-4EFEVVD\jrsar <jr.sarcilla07@gmail.com>
2026-02-07 17:21:14 +0800

    chore(build): remove compiled types

--- packages/core/src/domain/constants/validation-messages.d.ts
+++
@@ -1,6 +0,0 @@
-import type { ValidationRule } from '../schema/types';
-export declare const DEFAULT_VALIDATION_MESSAGES: Record<
-	ValidationRule['type'],
-	string
->;
-//# sourceMappingURL=validation-messages.d.ts.map
